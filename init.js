9d3c667dea28b9db3642b19f5cd317adb30a1109
AngelQnc <88949903+AngelQnc@users.noreply.github.com>
2021-11-18 12:10:19 -0600

    Add files via upload

---
+++ init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyDXpmmnEiXocV7NNljPc4beC_u07u_w6-M",
+  authDomain: "chat-e0547.firebaseapp.com",
+  projectId: "chat-e0547",
+  storageBucket: "chat-e0547.appspot.com",
+  messagingSenderId: "942798085030",
+  appId: "1:942798085030:web:aed3bfeb84a8aa003e8d67",
+  measurementId: "G-VM3NHV7YXZ"
+});
